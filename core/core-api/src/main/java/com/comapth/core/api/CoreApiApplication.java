d29beb32729d5f876bf8c99d78f05b5a05f06ded
kimday0326 <ktnqkr01@gmail.com>
2024-07-22 17:46:45 +0900

    initial commit

---
+++ core/core-api/src/main/java/com/comapth/core/api/CoreApiApplication.java
@@ -0,0 +1,15 @@
+package com.comapth.core.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+
+@ConfigurationPropertiesScan
+@SpringBootApplication(scanBasePackages = {"com.compath"})
+public class CoreApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreApiApplication.class, args);
+	}
+
+}
